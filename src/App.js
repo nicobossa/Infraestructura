43775141535624b484ad3235483e694cb232af58
Nicolás Bossa <nicolasbofo@unisabana.edu.co>
2020-04-18 00:34:37 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            TEST.
+            SOY EL INICIAL
           </p>
         </header>
       </div>
